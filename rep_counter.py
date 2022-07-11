68b6bba0d7ba979d5ba5b2650a364f469e929d9c
Babaganoush07 <94538153+Babaganoush07@users.noreply.github.com>
2022-07-11 15:47:21 -0700

    Add files via upload

---
+++ rep_counter.py
@@ -0,0 +1,35 @@
+def inputNumber(message):
+  while True:
+    try:
+       userInput = int(input(message))       
+    except ValueError:
+       print("Enter a number.")
+       continue
+    else:
+       return userInput 
+       break
+
+def progress(total,goal):
+  print(f"Total: {total} / {goal}")
+  percent = round((total/goal)*100,1)
+  print("▓" * int(int(percent)/10) + " " * (10-int(int(percent)/10)) + str(percent) + "%")
+  
+#MAIN PROGRAM STARTS HERE:
+goal = inputNumber("Goal: ")
+total = 0
+import os
+
+while total != goal:
+  # Do this every time
+  #os.system("clear")
+  print("Rep Counter")
+  progress(total, goal)
+  # If reps is less than goal keep looping
+  if total >= goal:
+    break
+  else:
+    reps = inputNumber("Reps: ")
+    total += reps
+
+if total <= goal:
+  progress(total,goal)
